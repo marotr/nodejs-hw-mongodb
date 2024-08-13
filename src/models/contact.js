e4386520096b4db1d2a0a42f76e04e566522df27
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-08-13 15:27:16 +0300

    corrected _v

--- src/models/contact.js
+++ src/models/contact.js
@@ -1,19 +1,57 @@
+// import mongoose from 'mongoose';
+
+// const contactSchema = new mongoose.Schema(
+//   {
+//     name: { type: String, required: true },
+//     phoneNumber: { type: String, required: true },
+//     email: { type: String, required: false },
+//     isFavourite: { type: Boolean, default: false },
+//     contactType: {
+//       type: String,
+//       enum: ['work', 'home', 'personal'],
+//       required: true,
+//       default: 'personal',
+//     },
+//   },
+//   { timestamps: true },
+// );
+
+// const Contact = mongoose.model('Contact', contactSchema);
+
+// export default Contact;
+
+
 import mongoose from 'mongoose';
 
 const contactSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    phoneNumber: { type: String, required: true },
-    email: { type: String, required: false },
-    isFavourite: { type: Boolean, default: false },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
     contactType: {
       type: String,
       enum: ['work', 'home', 'personal'],
       required: true,
       default: 'personal',
     },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  { timestamps: true },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
 );
 
 const Contact = mongoose.model('Contact', contactSchema);
