9fee40beeb908da9674eb1617c46a65fcb67d707
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-08-14 20:21:41 +0300

    Update parseFilterParams.js

--- src/utils/parseFilterParams.js
+++ src/utils/parseFilterParams.js
@@ -1,15 +1,41 @@
+// const parseContactType = (type) => {
+//   const isString = typeof type === 'string';
+//   if (!isString) return;
+//   const isContactType = (type) => ['personal', 'home', 'work'].includes(type);
+
+//   if (isContactType(type)) return type;
+// };
+
+// const parseIsFavourite = (isFavourite) => {
+//   if (typeof isFavourite === 'boolean') {
+//     return isFavourite;
+//   }
+//   return null;
+// };
+
+// export const parseFilterParams = (query) => {
+//   const { type, isFavourite } = query;
+
+//   const parsedContactType = parseContactType(type);
+//   const parsedIsFavourite = parseIsFavourite(isFavourite);
+
+//   return {
+//     type: parsedContactType,
+//     isFavourite: parsedIsFavourite,
+//   };
+// };
+
+
 const parseContactType = (type) => {
   const isString = typeof type === 'string';
   if (!isString) return;
-  const isContactType = (type) => ['personal', 'home', 'work'].includes(type);
-
-  if (isContactType(type)) return type;
+  const validTypes = ['personal', 'home', 'work'];
+  return validTypes.includes(type) ? type : undefined;
 };
 
 const parseIsFavourite = (isFavourite) => {
-  if (typeof isFavourite === 'boolean') {
-    return isFavourite;
-  }
+  if (isFavourite === 'true') return true;
+  if (isFavourite === 'false') return false;
   return null;
 };
 
