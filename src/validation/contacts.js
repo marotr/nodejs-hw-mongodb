cdd902a1159349f1f69e8deea9beba06b9a13bb2
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-08-15 15:22:58 +0300

    joi updates

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,32 +1,29 @@
 import Joi from 'joi';
-import JoiPhoneNumber from 'joi-phone-number';
 
-const JoiExtended = Joi.extend(JoiPhoneNumber);
-
-export const createContactSchema = JoiExtended.object({
-  name: JoiExtended.string().min(3).max(20).required().messages({
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required().messages({
     'string.base': 'Name should be a string',
     'string.min': 'Name should have at least {#limit} characters',
     'string.max': 'Name should have at most {#limit} characters',
     'any.required': 'Name is required',
   }),
-  phoneNumber: JoiExtended.string()
-    .phoneNumber({ format: 'national' })
-    .required()
-    .messages({
-      'string.base': 'Phone should be a string',
-      'phoneNumber.format': 'Phone should be in a national format',
-      'any.required': 'Phone is required',
-    }),
-  email: JoiExtended.string().min(3).max(20).email().required().messages({
+  phoneNumber: Joi.string()
+  .pattern(/^\d+$/) 
+  .required()
+  .messages({
+    'string.base': 'Phone number should be a string',
+    'string.pattern.base': 'Phone number should contain only digits',
+    'any.required': 'Phone number is required',
+  }),
+  email: Joi.string().min(3).max(20).email().required().messages({
     'string.base': 'Email should be a string',
     'string.email': 'Email must be a valid email',
     'any.required': 'Email is required',
   }),
-  isFavourite: JoiExtended.boolean().messages({
+  isFavourite: Joi.boolean().messages({
     'boolean.base': 'isFavourite should be a boolean',
   }),
-  contactType: JoiExtended.string()
+  contactType: Joi.string()
     .valid('work', 'home', 'personal')
     .required()
     .messages({
@@ -36,29 +33,29 @@ export const createContactSchema = JoiExtended.object({
     }),
 });
 
-export const updateContactSchema = JoiExtended.object({
-  name: JoiExtended.string().min(3).max(20).messages({
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).messages({
     'string.base': 'Name should be a string',
     'string.min': 'Name should have at least {#limit} characters',
     'string.max': 'Name should have at most {#limit} characters',
 
   }),
-  phoneNumber: JoiExtended.string()
-    .phoneNumber({ format: 'national' })
-    .messages({
-      'string.base': 'Phone should be a string',
-      'phoneNumber.format': 'Phone should be in a national format',
-      
-    }),
-  email: JoiExtended.string().min(3).max(20).email().messages({
+  phoneNumber: Joi.string()
+  .pattern(/^\d+$/) 
+  .messages({
+    'string.base': 'Phone number should be a string',
+    'string.pattern.base': 'Phone number should contain only digits',
+  
+  }),
+  email: Joi.string().min(3).max(20).email().messages({
     'string.base': 'Email should be a string',
     'string.email': 'Email must be a valid email',
     
   }),
-  isFavourite: JoiExtended.boolean().messages({
+  isFavourite: Joi.boolean().messages({
     'boolean.base': 'isFavourite should be a boolean',
   }),
-  contactType: JoiExtended.string()
+  contactType: Joi.string()
     .valid('work', 'home', 'personal')
     .messages({
       'string.base': 'Contact type should be a string',
