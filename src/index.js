af84cd93a5f0a03b08a8c8acc8011cc1adb4e19d
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-08-19 11:44:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,24 +3,25 @@ dotenv.config();
 
 import setupServer from './server.js';
 import initMongoConnection from './db/initMongoConnection.js';
-import Contact from './models/contact.js';
+// import Contact from './models/contact.js';
 
 
 const startServer = async () => {
   try {
     await initMongoConnection();
     console.log('Connected to MongoDB');
+    
 
-    const testContact = new Contact({
-      name: "Test Contact",
-      phoneNumber: "123-456-7890",
-      email: "test@example.com",
-      isFavourite: true,
-      contactType: "work"
-    });
+    // const testContact = new Contact({
+    //   name: "Test Contact",
+    //   phoneNumber: "123-456-7890",
+    //   email: "test@example.com",
+    //   isFavourite: true,
+    //   contactType: "work"
+    // });
 
-    await testContact.save();
-    console.log('Test contact saved:', testContact);
+    // await testContact.save();
+    // console.log('Test contact saved:', testContact);
 
   
     setupServer();
