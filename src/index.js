696b01957d320ceb9852da4fb43a9f3d08357688
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-08-19 12:39:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 import setupServer from './server.js';
 import initMongoConnection from './db/initMongoConnection.js';
-// import Contact from './models/contact.js';
+
 
 
 const startServer = async () => {
@@ -12,17 +12,7 @@ const startServer = async () => {
     console.log('Connected to MongoDB');
     
 
-    // const testContact = new Contact({
-    //   name: "Test Contact",
-    //   phoneNumber: "123-456-7890",
-    //   email: "test@example.com",
-    //   isFavourite: true,
-    //   contactType: "work"
-    // });
-
-    // await testContact.save();
-    // console.log('Test contact saved:', testContact);
-
+ 
   
     setupServer();
   } catch (error) {
