5597f46dc6ce76a361cc891f526340c019be7f58
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-08-20 05:29:32 +0300

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -4,8 +4,8 @@ const usersSchema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    userId:{type:String, required: true}
+    password: { type: String, required: true }
+    
   },
   { timestamps: true, versionKey: false },
 );
